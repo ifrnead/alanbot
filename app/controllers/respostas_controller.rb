e3dc6b177a90af985cfd1ddc64d0235503fc62c6
Jalerson Lima <jalerson@gmail.com>
2017-12-06 13:58:54 -0300

    Implementação da resposta.

--- app/controllers/respostas_controller.rb
+++ app/controllers/respostas_controller.rb
@@ -1,7 +1,10 @@
+require 'json'
+
 class RespostasController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [ :callback ]
 
   def callback
+    @resposta = Resposta.buscar(JSON.parse(params))
     render 'callback.json'
   end
 
--- app/models/resposta.rb
+++ app/models/resposta.rb
@@ -3,4 +3,11 @@ class Resposta < ApplicationRecord
   belongs_to :informacao
 
   validates :informacao, :alvo, :mensagem, presence: true
+
+  def buscar(params)
+    alvo = Alvo.find_by_nome(params['result']['parameters']['alvos'])
+    informacao = Informacao.find_by_tipo(params['result']['parameters']['informacoes'])
+
+    self.where(alvo: alvo, informacao: informacao)
+  end
 end

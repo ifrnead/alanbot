0ab41abf213045312a5ce34a4951605709af0431
Jalerson Lima <jalerson.lima@ifrn.edu.br>
2017-12-13 23:09:26 +0000

    criacao do CRUD, POREM O DESTROY N FUNCIONA

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+ 
+    resources :informacoes
+
+    resources :alvos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'respostas/callback', to: 'respostas#callback'
 end

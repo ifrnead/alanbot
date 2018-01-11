9a113e557369527db12c2aa2d3156d2ba57ada0b
Jalerson Lima <jalerson@gmail.com>
2018-01-10 22:09:47 -0300

    Reorganizando apresentação do gráfico.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,4 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
-
-//= require Chart.bundle
 //= require chartkick
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,12 @@
 module DashboardHelper
+
+  def grafico_mensagens_atentidas
+    pie_chart({
+        "Mensagens atendidas" => Mensagem.atendidas.size,
+        "Mensagens não atendidas" => Mensagem.nao_atendidas.size
+      }, colors: [ 'green', 'red' ],
+      library: { pieSliceText: 'value-and-percentage' }
+    )
+  end
+
 end

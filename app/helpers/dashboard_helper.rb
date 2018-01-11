ca19882c1f0ef6a5ebf9023bcf6e3f843852fb7f
Jalerson Lima <jalerson.lima@ifrn.edu.br>
2018-01-11 01:38:35 +0000

    criacao de grafico de mensagens por dia

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -9,4 +9,21 @@ module DashboardHelper
     )
   end
 
+  def grafico_mensagens_dia
+    line_chart ([
+        {
+            'name' => 'Total de Mensagens',
+            'data' => Mensagem.group_by_day(:created_at).count
+        },
+         {
+            'name' => 'Mensagens Atendidas',
+            'data' => Mensagem.atendidas.group_by_day(:created_at).count
+        },
+         {
+            'name' => 'Mensagens Não Atendidas',
+            'data' => Mensagem.nao_atendidas.group_by_day(:created_at).count
+        }
+    ]
+    ) 
+  end
 end
